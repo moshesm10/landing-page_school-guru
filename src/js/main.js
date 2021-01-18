91e4a6b44586b4082777cda5aa5b62bda55eefc1
Alex <moshesm@yandex.ru>
2021-01-18 23:55:33 +0300

    clean up

--- src/js/main.js
+++ src/js/main.js
@@ -6,29 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const scroll = new SmoothScroll('a[href*="#"]', {
         speed: 200
     });
-
-    // Отправка email на почту
-    const form = document.querySelector('.callback-block__content-email'),
-          formButton = form.querySelector('.callback-block__content-email__button');
-
-          formButton.addEventListener('click', async (e) => {
-            e.preventDefault();
-
-            const email = document.querySelector('.callback-block__content-email__input').value;
-            if (email) {
-                const formData = new FormData();
-                formData.append('email', email);
-
-                let res = await fetch('./mail.php', {
-                    method: 'POST',
-                    body: formData
-                });
-
-                
-                let result = await res.text();//.json();
-                console.log(result);
-            }
-          });
     
     // Бургер меню
     const burgerButton = document.querySelector('.menu__burger-button'),

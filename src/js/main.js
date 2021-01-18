f489cca1b308ff573caa45c4a6cfd12a34a09d1a
Alex <moshesm@yandex.ru>
2021-01-18 23:44:28 +0300

    burger logic

--- src/js/main.js
+++ src/js/main.js
@@ -30,6 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
             }
           });
     
-    
+    // Бургер меню
+    const burgerButton = document.querySelector('.menu__burger-button'),
+          menuWrapper = document.querySelector('.menu__wrapper');
+    burgerButton.addEventListener('click', () => {
+        menuWrapper.style.left = 0;
+        menuWrapper.addEventListener('click', () => {
+            menuWrapper.style.left = '-100vw';
+        });
+    });
 
 });
